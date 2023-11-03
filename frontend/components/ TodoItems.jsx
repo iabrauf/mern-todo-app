f7a95edc163d964de7901b31b6a9ee902bed0cb9
iabrauf <abdul.rauf@coderzhunt.com>
2023-11-03 12:55:44 +0500

    update delete loader

--- frontend/components/ TodoItems.jsx
+++ frontend/components/ TodoItems.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import TodoItem from './TodoItem';
 import { Circles } from 'react-loader-spinner';
 
--- frontend/components/TodoItem.jsx
+++ frontend/components/TodoItem.jsx
@@ -3,7 +3,6 @@ import CheckCircleIcon from '@/assets/icons/CheckCircleIcon';
 import DotIcon from '@/assets/icons/DotIcon';
 import axios from 'axios';
 import { ColorRing } from 'react-loader-spinner';
-import { Circles } from 'react-loader-spinner';
 
 const TodoItem = ({ todo, getTodos }) => {
     const [isCompleted, setIsCompleted] = useState(todo.isCompleted);
@@ -69,61 +68,62 @@ const TodoItem = ({ todo, getTodos }) => {
 
     return (
         <>
-            {deleteLoader ? (
-                <div className='flex item-center justify-center p-3'>
-                    <Circles
-                        height="60"
-                        width="60"
-                        color="#615336"
-                        ariaLabel="circles-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        visible={true}
-                    />
-                </div>
-            ) : (
-                <>
-                    <button
-                        className="relative focus:outline-none rounded-[7px] font-medium text-sm px-2 py-2.5 text-center inline-flex items-center w-full border-b border-gray-400"
-                        type="button"
-                    >
-                        <span className='w-6 h-6' onClick={updateTask}>
-                            {!updateLoader ? (
-                                <CheckCircleIcon
-                                    stroke='#615336'
-                                    fill={isCompleted ? '#615336' : 'transparent'}
-                                />
-                            ) : (
-                                <ColorRing
-                                    visible={true}
-                                    height={24}
-                                    width={24}
-                                    ariaLabel="Loading..."
-                                    colors={['#7c6f5a']}
-                                />
-                            )}
-                        </span>
-                        {todo.name}
-                        <span className='w-6 h-6 absolute left-[88%]' onClick={() => setShowDetails(!showDetails)}>
-                            <DotIcon />
-                        </span>
-                    </button>
-                    {showDetails && (
-                        <div className='w-full bg-white flex flex-col px-2 py-2.5'>
-                            <p><span className='font-bold'>Completed: </span>{isCompleted ? 'Completed' : 'Not Completed'}</p>
-                            <p><span className='font-bold'>Create At: </span>{formattedDate}</p>
-                            <button
-                                onClick={deleteTodo}
-                                className='text-center w-full bg-red-300 rounded-md p-1.5 mt-1.5 text-red-700'>
+            <button
+                className="relative focus:outline-none rounded-[7px] font-medium text-sm px-2 py-2.5 text-center inline-flex items-center w-full border-b border-gray-400"
+                type="button"
+            >
+                <span className='w-6 h-6' onClick={updateTask}>
+                    {!updateLoader ? (
+                        <CheckCircleIcon
+                            stroke='#615336'
+                            fill={isCompleted ? '#615336' : 'transparent'}
+                        />
+                    ) : (
+                        <ColorRing
+                            visible={true}
+                            height={24}
+                            width={24}
+                            ariaLabel="Loading..."
+                            colors={['#7c6f5a']}
+                        />
+                    )}
+                </span>
+                {todo.name}
+                <span className='w-6 h-6 absolute left-[88%]' onClick={() => setShowDetails(!showDetails)}>
+                    <DotIcon />
+                </span>
+            </button>
+            {showDetails && (
+                <div className='w-full bg-white flex flex-col px-2 py-2.5'>
+                    <p><span className='font-bold'>Completed: </span>{isCompleted ? 'Completed' : 'Not Completed'}</p>
+                    <p><span className='font-bold'>Create At: </span>{formattedDate}</p>
+                    <div>
+                        {!deleteLoader ? (
+                            <button onClick={deleteTodo}
+                                className='text-center w-full bg-red-300 rounded-md p-1.5 mt-1.5 text-red-700'
+                            >
                                 Delete
                             </button>
-                        </div>
-                    )}
-                </>
-            )}
+                        ) : (
+                            <div className='flex items-center justify-center w-full bg-red-300 rounded-md p-1.5 mt-1.5 text-red-700'>
+                                <ColorRing
+                                    visible={true}
+                                    height={25}
+                                    width={25}
+                                    ariaLabel="Loading..."
+                                    colors={["#DC143C", "#D70040", '#DC143C', "#D70040", '#DC143C']}
+                                />
+                            </div>
+                        )
+                        }
+                    </div>
+                </div>
+            )
+            }
         </>
-
-    );
-};
-
+    )
+}
 export default TodoItem;
+
+
+
